1eeccafe101a22184d2e0ce7361af3d7e8da3e45
Trevor Fullwood <tfullwood@instructure.com>
2019-08-30 08:36:57 -0600

    Finalize for hackathon demo

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 const express = require('express');
 
-//import routes
-// const userRoutes = require('./users');
 const agentRoutes = require('./agents');
-
 
 const router = express.Router();
 
-//mount routes
-// router.use('/users', userRoutes); //Users
 router.use('/agents', agentRoutes)
 
 module.exports = router;
